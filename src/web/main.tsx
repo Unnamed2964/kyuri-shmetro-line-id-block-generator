80834feb4be9e971757aefb24b8e91b8f6088691
Umamichi <umamichi@outlook.com>
2026-04-30 01:13:00 +0800

    Integrate @umamichi-ui/common-css

--- src/web/main.tsx
+++ src/web/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@umamichi-ui/common-css';
+import '@umamichi-ui/common-css/article.css';
 import { App } from './App';
 import './styles.css';
 
